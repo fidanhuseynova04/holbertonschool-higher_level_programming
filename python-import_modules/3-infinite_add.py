630046a0b86636f48354e4c8df7c13869397ac8f
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-05-19 19:20:15 +0000

    3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    n = len(sys.argv)
+    total_sum = 0
+    if n == 1:
+        print(0)
+    else:
+        for i in range(1, n):
+            total_sum += int(sys.argv[i])
+        print("{}".format(total_sum))
