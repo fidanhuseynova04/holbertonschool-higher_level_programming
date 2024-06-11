42f282b746bc7db1dbd39ed77b2002a67292b4a9
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-06-11 13:15:52 +0000

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module containing read_file function"""
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
