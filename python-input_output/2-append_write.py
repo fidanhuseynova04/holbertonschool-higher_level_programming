219483ec9b04db96f02b4ddb68da02d61e53f7fd
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-06-11 13:51:14 +0000

    2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string at the end of a text file (UTF8) and returns the number of characters added."""
+    """The append_file function"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
