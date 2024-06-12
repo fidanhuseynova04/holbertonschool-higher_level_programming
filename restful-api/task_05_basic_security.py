e3214f1ed29f444a3f0423c36b2c0c5f51f4730e
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-06-12 08:57:29 +0000

    task_05_basic_security.py

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 """Module for secure API"""
 from flask import Flask, jsonify, request
-from flask_httpauth import HTTPBasicAuth
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_httpauth import HTTPBasicAuth
+from flask_jwt_extended import (JWTManager, create_access_token,
+                                jwt_required, get_jwt_identity)
 
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()
 app.config['SECRET_KEY'] = 'your_secret_key_here'
+auth = HTTPBasicAuth()
 jwt = JWTManager(app)
 
 users = {
-    "user1": {"username": "user1", "password": generate_password_hash("password1"), "role": "user"},
-    "admin1": {"username": "admin1", "password": generate_password_hash("password2"), "role": "admin"}
+    "user1": {
+        "username": "user1",
+        "password": generate_password_hash("password"),
+        "role": "user"
+    },
+    "admin1": {
+        "username": "admin1",
+        "password": generate_password_hash("password"),
+        "role": "admin"
+    }
 }
 
 
@@ -36,12 +45,11 @@ def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-
     user = users.get(username)
     if user and check_password_hash(user['password'], password):
-        access_token = create_access_token(identity={'username': username, 'role': user['role']})
+        access_token = create_access_token(identity={'username': username,
+                                                     'role': user['role']})
         return jsonify(access_token=access_token)
-
     return jsonify({"error": "Invalid credentials"}), 401
 
 
@@ -55,7 +63,7 @@ def jwt_protected():
 @jwt_required()
 def admin_only():
     current_user = get_jwt_identity()
-    if current_user['role'] == 'admin':
+    if current_user['role'] != 'admin':
         return jsonify({"error": "Admin access required"}), 403
     return "Admin Access: Granted"
 
@@ -83,6 +91,7 @@ def handle_revoked_token_error(err):
 @jwt.needs_fresh_token_loader
 def handle_needs_fresh_token_error(err):
     return jsonify({"error": "Fresh token required"}), 401
+
 
 if __name__ == "__main__":
     app.run()
