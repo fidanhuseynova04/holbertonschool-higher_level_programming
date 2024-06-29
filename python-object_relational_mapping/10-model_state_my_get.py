632733df53ecd735b6a95193cfda42b5e423d6c1
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-06-29 21:54:00 +0000

    10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-"""script that prints the State object with
-the name passed as argument from the database hbtn_0e_6_usa"""
+"""
+This script connects to a MySQL database
+"""
 
 import sys
 from model_state import Base, State
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+
+def main():
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
+    st_name = sys.argv[4]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(usr, pwd, db_name))
+
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).where(State.name.like(sys.argv[4]))\
-                   . order_by(State.id).first()
-    try:
-        print("{}".format(state.id))
-    except Exception:
-        print("Not found")
-    session.close()
+
+    for state in session.query(State).filter(State.name == st_name).all():
+        print(state.id)
+        return
+    print("Not found")
+
+
+if __name__ == "__main__":
+    main()
