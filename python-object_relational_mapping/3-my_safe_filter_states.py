29c0c65cb1d687edbcda5dc899d286342ba6a2cf
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-06-29 21:21:54 +0000

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,12 +1,35 @@
 #!/usr/bin/python3
-"""displays all values where name matches the argument"""
+"""
+This script connects to a MySQL database
+"""
 
-import MySQLdb
 import sys
-import sqlalchemy
+import MySQLdb as mysql
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-    [print(state) for state in cur.fetchall() if state[1] == sys.argv[4]]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = mysql.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    cursor = db.cursor()
+    cursor.execute("""
+    SELECT *
+    FROM states
+    WHERE BINARY name = %s AND name IS NOT NULL
+    ORDER BY id ASC;""".format(state_name_searched), (state_name_searched, ))
+    result = cursor.fetchall()
+
+    for x in result:
+        print(x)
+
+    cursor.close()
+    db.close()
