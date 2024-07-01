3125a0b49dc2204353ccd19f3e61657cfbd0b662
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-07-01 12:16:50 +0000

    10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
 """
-This script connects to a MySQL database
+Script that prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa
 """
 
+
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
-
-def main():
+if __name__ == "__main__":
     usr = sys.argv[1]
     pwd = sys.argv[2]
     db_name = sys.argv[3]
-    st_name = sys.argv[4]
+    srch_state = sys.argv[4]
 
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(usr, pwd, db_name))
 
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
+    state = session.query(State).filter(State.name == srch_state).first()
 
-    for state in session.query(State).filter(State.name == st_name).all():
+    if state:
         print(state.id)
-        return
-    print("Not found")
-
-
-if __name__ == "__main__":
-    main()
+    else:
+        print("Not found")
+    session.close()
