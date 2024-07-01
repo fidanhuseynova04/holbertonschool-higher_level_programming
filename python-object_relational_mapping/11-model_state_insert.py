0e278836da5cab6746b8586098b8e2347ed52096
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-07-01 12:20:26 +0000

    11-model_state_insert.py

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-This script connects to a MySQL database
+Script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
 """
 
+
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
-
-def main():
+if __name__ == "__main__":
     usr = sys.argv[1]
     pwd = sys.argv[2]
     db_name = sys.argv[3]
@@ -18,16 +18,12 @@ def main():
                            .format(usr, pwd, db_name))
 
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
+    new_state = State(name="Louisiana")
 
-    new_state = State(name='Louisiana')
     session.add(new_state)
     session.commit()
     print(new_state.id)
+
     session.close()
-
-
-if __name__ == "__main__":
-    main()
